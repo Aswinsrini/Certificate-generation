db5eda21b97355e3674f4172c0ea31187c347d5e
ChowdriS <114232036+ChowdriS@users.noreply.github.com>
2023-10-01 18:24:31 +0530

    patch work

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import list from "../assets/college_list.json";
 import { Link } from "react-router-dom";
 const HomePage = () => {
@@ -7,40 +7,50 @@ const HomePage = () => {
   ));
 
   return (
-    <div className="p-6">
+    <div className="flex flex-col items-center justify-center">
+    <div className="p-6 w-5/6">
+        <div className="flex flex-col items-end justify-center">
+        <p>Are you Admin?</p>
+        <Link to="/admin" className="mx-3">Login here</Link>
+        </ div>
+      <div className="flex justify-center">
+        <b className="text-blue-600 text-center my-5 font-bold text-5xl underline  italic">-Training Trains-</b> 
+      </div>
       <div>
-        <b className="text-blue-600">Training Trains</b> Intern and Implant
-        Training program <br />
-        Are you Admin?
-        <Link to="/admin">Login here</Link>
+        <div className="flex items-center justify-center text-sky-600">
+        <p className="mx-12">Intern and Implant</p>
+        <p className="mx-12">Training program</p> <br />
+        </div>
       </div>
       <form className="mt-4" action="/submit" method="post">
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-gray-700">
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="name" className="text-gray-900 text-xl">
             Name
           </label>
           <input
             type="text"
             name="name"
             id="name"
-            className="border rounded-md p-2 w-full"
+            className="border border-solid border-black rounded-md p-2 mx-2 w-[75%]"
+            placeholder="Enter the Name"
             required
           />
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700">
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="email" className="text-gray-900 text-xl">
             Email
           </label>
           <input
             type="email"
             name="email"
             id="email"
-            className="border rounded-md p-2 w-full"
+            placeholder="Enter the Email"
+            className="border border-solid border-black rounded-md p-2 mx-2 w-[75%]"
             required
           />
         </div>
-        <div className="mb-4">
-          <label htmlFor="number" className="block text-gray-700">
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="number" className="text-gray-900 text-xl">
             Phone number
           </label>
           <input
@@ -49,58 +59,62 @@ const HomePage = () => {
             maxLength={10}
             name="number"
             id="number"
-            className="border rounded-md p-2 w-full"
+            placeholder="Enter the Phone Number"
+            className="border  border-solid border-black rounded-md p-2 mx-2 w-[75%]"
             required
           />
         </div>
-        <div className="mb-4">
-          <label htmlFor="college" className="block text-gray-700">
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="college" className="text-gray-900 text-xl">
             Select your college
           </label>
           <select
             name="college"
             id="college"
-            className="border rounded-md p-2 w-full"
+            className="border  border-solid border-black rounded-md p-2 mx-2 w-[75%]"
             required
           >
+          <option value="none">Select the College</option>
             {college}
           </select>
         </div>
-        <div className="mb-4">
-          <label htmlFor="intern" className="block text-gray-700">
-            Type of program
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="intern" className="text-gray-900 text-xl">
+            Type of Program
           </label>
           <select
             name="intern"
             id="intern"
-            className="border rounded-md p-2 w-full"
+            className="border  border-solid border-black rounded-md p-2 mx-2 w-[75%]"
           >
+            <option value="none">Select the Program</option>
             <option value="internship">Internship</option>
             <option value="implant">Implant</option>
           </select>
         </div>
-        <div className="mb-4">
-          <label htmlFor="course" className="block text-gray-700">
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="course" className="text-gray-900 text-xl">
             Select the Course
           </label>
           <select
             name="course"
             id="course"
-            className="border rounded-md p-2 w-full"
+            className="border  border-solid border-black rounded-md p-2 mx-2 w-[75%]"
           >
+            <option value="none">Select the Course</option>
             <option value="full-stack">Full Stack web developer</option>
             <option value="mobile">Mobile app developer</option>
             <option value="cyber">Cyber Security</option>
           </select>
         </div>
-        <div className="mb-4">
-          <label htmlFor="duration" className="block text-gray-700">
+        <div className="mb-4 flex items-center justify-between">
+          <label htmlFor="duration" className="text-gray-900 text-xl">
             Duration in months
           </label>
           <select
             name="duration"
             id="duration"
-            className="border rounded-md p-2 w-full"
+            className="border  border-solid border-black rounded-md p-2 mx-2 w-[75%]"
             required
           >
             <option value="1">1</option>
@@ -111,13 +125,16 @@ const HomePage = () => {
             <option value="6">6</option>
           </select>
         </div>
+        <div className="flex items-center justify-center">
         <button
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded text-lg"
           type="submit"
         >
           Submit
         </button>
+        </div>
       </form>
+    </div>
     </div>
   );
 };
